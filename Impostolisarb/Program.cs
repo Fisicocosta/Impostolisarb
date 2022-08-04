1fcd2858aaa3ac886a289ea6fb0646b3104fd822
Costa <Costa@DELL>
2022-08-04 18:46:55 -0300

    Adicionar arquivos de projeto.

---
+++ Impostolisarb/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Globalization;
+
+Console.WriteLine("----------------------------------------------------------------------Imposto de LISARB-----------------------------------------------------");
+
+double salario;
+
+double imposto;
+
+Console.WriteLine("Digite o valor do salário.");
+
+salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+if (salario == 0.0)
+
+{
+    Console.WriteLine("Isento");
+
+}
+
+else if (salario > 0.0 && salario <= 2000.00)
+
+{
+    Console.WriteLine("Isento");
+
+}
+
+else if (salario > 2000.00 && salario <= 3000.00)
+
+{
+
+    imposto = (salario - 2000.00) * 0.08;
+    Console.WriteLine(" O imposto devido é de R$ " + imposto.ToString("F2", CultureInfo.InvariantCulture));
+
+}
+
+else if (salario > 3000.00 && salario <= 4500.00)
+
+{
+
+    imposto = (salario - 3000.00) * 0.18 + 1000.00 * 0.08;
+    Console.WriteLine("O imposto devido é de R$ " + imposto.ToString("F2", CultureInfo.InvariantCulture));
+
+}
+
+else if(salario > 4500.00)
+
+{
+
+    imposto = (salario - 4500.00) * 0.28 + 1500.00 * 0.18 + 1000 * 0.08;
+    Console.WriteLine("O imposto devido é de R$ " + imposto.ToString("F2", CultureInfo.InvariantCulture));
+
+}
